a501433cd437973d2bc1f14bffb739a429fc1731
“kawaimiki” <piiiiikann@gmail.com>
2021-12-15 06:50:19 +0000

    削除機能動作

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,32 +19,43 @@ class PostsController < ApplicationController
   def edit
   end
 
-  # POST /spots
+  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
-    if @post.save
-      redirect_to posts_url, notice: '作成されました'
-    else
-      render :new
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: "作成されました" }
+        format.json { render :posts_url, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  # PATCH/PUT /spots/1
+  # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    if @post.update(post_params)
-      redirect_to posts_url, notice: '更新されました'
-    else
-      render :edit
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "更新されました" }
+        format.json { render :posts_url, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  # DELETE /spots/1
+  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
-    redirect_to posts_url, notice: '削除しました'
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "削除しました" }
+      format.json { head :no_content }
+    end
   end
-
+  
   def search
     @results = @q.result
   end
